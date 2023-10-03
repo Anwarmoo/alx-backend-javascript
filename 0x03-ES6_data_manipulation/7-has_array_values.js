cd5d0c0edbe683992e9be79a2300cdc2bf3d38a4
MOHAMED ADAN <anwarmoh389@gmail.com>
2023-10-03 00:36:24 +0000

    0x03. ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,16 @@
+/*
+ * check if all elements in an array are
+ * also present in a set
+*/
+
+export default function hasValuesFromArray(set, array) {
+  if (!(set instanceof Set)) {
+    throw new TypeError(`${set} is not a valid set`);
+  }
+  if (!Array.isArray(array)) {
+    throw new TypeError(`${array} is not a valid array`);
+  }
+
+  const bools = array.map((elem) => set.has(elem));
+  return bools.every((item) => Boolean(item));
+}
